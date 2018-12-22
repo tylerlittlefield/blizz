b7cde16d1c6615f99e6cd6414ef017442e5c70cd
tylurp <tylurp1@gmail.com>
2018-12-21 16:18:04 -0800

    Add comments to see if linguist interprets the tests as R code, not rebol.
    src: https://github.com/github/linguist/issues/3190

--- tests/testthat/test-fetch_creds.R
+++ tests/testthat/test-fetch_creds.R
@@ -1,9 +1,15 @@
 context("test-fetch_creds")
 
 test_that("fetch_creds works", {
+
+  # Expect a list
   expect_type(fetch_creds(), "list")
+
+  # Expect that the list elements are named correctly
   expect_equal(names(fetch_creds()[1]), "id")
   expect_equal(names(fetch_creds()[2]), "secret")
   expect_equal(names(fetch_creds()[3]), "token")
+
+  # Expect an error when unneeded argument is given
   expect_error(fetch_creds("no args allowed"))
 })
