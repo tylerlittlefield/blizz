51dc8bd716c9315f20adf3649bd121ec716cd2cf
tylurp <tylurp1@gmail.com>
2019-01-08 09:31:40 -0800

    minor change

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,6 @@
 # Refresh the auth token everytime the library is loaded
 .onLoad <- function(libname, pkgname) {
-  if(nchar(Sys.getenv("BLIZZARD_AUTH_TOKEN")) > 1)
+  if(nchar(Sys.getenv("BLIZZARD_AUTH_TOKEN")) > 1) {
   blizz_auth(refresh = TRUE)
+  }
 }
