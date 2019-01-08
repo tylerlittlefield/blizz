8629758ee97f4806ea1dae961dfd330f77c32d93
tylurp <tylurp1@gmail.com>
2019-01-08 09:19:36 -0800

    change .onLoad function and update description

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 # Refresh the auth token everytime the library is loaded
 .onLoad <- function(libname, pkgname) {
-  if(length(Sys.getenv("BLIZZARD_AUTH_TOKEN")) == 1)
+  if(nchar(Sys.getenv("BLIZZARD_AUTH_TOKEN")) > 1)
   blizz_auth(refresh = TRUE)
 }
